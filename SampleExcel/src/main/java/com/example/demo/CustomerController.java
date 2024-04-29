484f9c9304c9bff184d5ad4cbb89efe0437b15a9
RaviVasantha <162537642+RaviVasantha@users.noreply.github.com>
2024-04-29 15:49:34 +0530

    Add files via upload

---
+++ SampleExcel/src/main/java/com/example/demo/CustomerController.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+	 @Autowired
+	    private CustomerService cs;
+
+	    @RequestMapping(path = "feedCustomerData")
+	    public ResponseEntity<String> setDataInDB() {
+	        try {
+	            cs.saveCustomerData();
+	            return ResponseEntity.ok("Customer data saved successfully.");
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred while saving customer data.");
+	        }
+	    }
+	    @GetMapping(value="/customers")
+	    public Iterable<Customer> getAllCustomers() {
+	        return cs.getAllCustomers();
+	    }
+
+}
