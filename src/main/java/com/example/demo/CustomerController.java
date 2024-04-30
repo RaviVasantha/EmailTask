3e3932a50a660e826dae5ef3a234d97720373025
RaviVasantha <162537642+RaviVasantha@users.noreply.github.com>
2024-04-30 10:45:09 +0530

    Update CustomerController.java

--- src/main/java/com/example/demo/CustomerController.java
+++ src/main/java/com/example/demo/CustomerController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class CustomerController {
@@ -21,5 +22,8 @@ public class CustomerController {
 	            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred while saving customer data.");
 	        }
 	    }
-
+	       @GetMapping(value="/customers")
+		public Iterable<Customer> getAllCustomers() {
+	        return cs.getAllCustomers();
+	    }
 }
